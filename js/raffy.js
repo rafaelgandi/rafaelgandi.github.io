f5542732b4ab6adecee4df50728d67f39effffa3
Raffy <rafael@sushidigital.com.au>
2015-11-27 14:26:30 +0800

    testing out frame and mobile detection

--- js/raffy.js
+++ js/raffy.js
@@ -16,16 +16,11 @@ requirejs.config({
     baseUrl: 'js',
     paths: {}
 });
-
-// Array.prototype.forEach() shiv //
-// See: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-Array.prototype.forEach||(Array.prototype.forEach=function(a,b){var c,d;if(this==null)throw new TypeError(" this is null or not defined");var e=Object(this),f=e.length>>>0;if(typeof a!="function")throw new TypeError(a+" is not a function");arguments.length>1&&(c=b),d=0;while(d<f){var g;d in e&&(g=e[d],a.call(c,g,d,e)),d++}});
 window.$ = window.jQuery; // Explicitly map the "$" sign to jQuery.
-define(['raffy/globals', 'raffy/navigate', 'util/isMobileFrame'], function (globals, navigate, isMobileFrame) {
+define(['util/Array.forEach', 'raffy/globals', 'raffy/navigate', 'util/isMobileFrame'], function (undef, globals, navigate, isMobileFrame) {
 	// Breakout of frame if in mobile and in frameset (.tk) //
 	isMobileFrame.ifTrue(function () {
-		alert('is in frame and mobile');
-		//self.location.href = globals.GITHUB_URI;
+		self.location.href = globals.GITHUB_URI;
 	});
 	// Load intro page scripts //
 	require(['raffy/introPage']);
--- js/util/isMobileFrame.js
+++ js/util/isMobileFrame.js
@@ -2,14 +2,6 @@
 	Module to check if website is being viewed inside a frame and is on mobile.
  */
 define(function () {
-	var screenWidth = 0,
-		inFrame = false,
-		mobileThreshold = 568;
-	
-	function getScreenWidth() {
-		return self.innerWidth || d.documentElement.clientWidth || d.getElementsByTagName('body')[0].clientWidth;
-	}
-	
 	function isInsideFrame() {
 		// See: http://stackoverflow.com/a/326076
 		try {
@@ -20,24 +12,15 @@ define(function () {
 	}
 	
 	function isMobile() {
+		// See: http://stackoverflow.com/a/14301832
 		return typeof window.orientation !== 'undefined';
 	}
-	
-	screenWidth = getScreenWidth();
-	inFrame = isInsideFrame();
-	
-	//console.warn(screenWidth);
-	//console.info(screen.width);
-	
-	alert(isMobile());
-	
+
 	return {
-		screenWidth: screenWidth,
-		inFrame: inFrame,
 		ifTrue: function (_callback) {
 			_callback = _callback || function () {};
 			// If is inside a frame and on mobile //
-			if (inFrame && (screenWidth <= mobileThreshold)) {
+			if (isInsideFrame() && isMobile()) {
 				_callback();
 			}
 		}
