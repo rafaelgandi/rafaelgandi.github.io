d7b543b6e66b64fa14823cfb429204dbb584bed9
raffy@work <raffy@vectto.com>
2019-03-13 15:08:26 +0800

    more updates

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 requirejs.config({
     baseUrl: '/js',
-	// See: http://stackoverflow.com/questions/13626715/load-timeout-for-module-domready-unnormalized2
-	// See: http://stackoverflow.com/questions/14279962/require-js-error-load-timeout-for-modules-backbone-jquerymobile
-	waitSeconds: 200
+    // See: http://stackoverflow.com/questions/13626715/load-timeout-for-module-domready-unnormalized2
+    // See: http://stackoverflow.com/questions/14279962/require-js-error-load-timeout-for-modules-backbone-jquerymobile
+    waitSeconds: 200
 });
 define((require) => {
 	"use strict";
